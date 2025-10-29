9a447ec381e60ec3b41c459ea4b35dcca2cc4719
myhmmt <muhammetgnc461@gmail.com>
2025-10-29 20:30:46 +0300

    Add caching mechanism to service worker
    Implement caching strategy in fetch event handler

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,25 @@
 self.addEventListener("install", () => {
   console.log("Service Worker: installed");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", () => {
+  console.log("Service Worker: active");
+  return self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).then((resp) => {
+          return caches.open("santiye-cache").then((cache) => {
+            cache.put(event.request, resp.clone());
+            return resp;
+          });
+        })
+      );
+    })
+  );
 });
