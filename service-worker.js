d2312d270d2d8f06cb427e382d1557a2d9867e04
myhmmt <muhammetgnc461@gmail.com>
2025-11-04 20:31:06 +0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", () => {
-  console.log("Service Worker: installed");
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
-});
