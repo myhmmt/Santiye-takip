5d39bc653f9910341b6001257656c45db3d4b75c
myhmmt <muhammetgnc461@gmail.com>
2025-11-11 17:11:18 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,24 @@
-const CACHE = "vista-v1.4";
+const CACHE_NAME = 'vista-v1.5.1';
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json",
-  "./assets/icon/icon-192.png",
-  "./assets/icon/icon-512.png",
-  "./assets/icon/apple-icon-152.png",
-  "./assets/icon/apple-icon-180.png",
-  "https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"
+  'index.html','style.css','app.js','manifest.json',
+  'assets/icons/icon-192.png','assets/icons/icon-512.png',
+  'assets/icons/apple-icon-152.png','assets/icons/apple-icon-180.png'
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (e) => {
+self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k))))
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", (e) => {
-  const { request } = e;
-  if (request.url.includes("firestore.googleapis.com")) return;
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(request).then(cached => 
-      cached || fetch(request).then(resp => {
-        const copy = resp.clone();
-        caches.open(CACHE).then(c => c.put(request, copy));
-        return resp;
-      }).catch(() => cached)
-    )
+    caches.match(e.request).then(cached => cached || fetch(e.request))
   );
 });
